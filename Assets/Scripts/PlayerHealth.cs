df30da8e6af4e14b273af087b5797f3b0c8578c6
s15050 <45424170+s15050@users.noreply.github.com>
2019-04-06 17:48:10 +0200

    Progress 2

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,16 +6,37 @@ using UnityEngine.UI;
 public class PlayerHealth : MonoBehaviour
 {
     public int lives = 3;
-    
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
+
+    private int noOfHits;
+    private Image[] hearts = new Image[3];
     // Start is called before the first frame update
     void Start()
     {
-        
+        noOfHits = 0;
+        hearts[0] = heart1;
+        hearts[1] = heart2;
+        hearts[2] = heart3;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("killersushi"))
+        {
+            hearts[noOfHits].color = Color.black;
+            noOfHits++;
+            if (noOfHits == lives)
+            {
+
+            }
+        }
     }
 }
