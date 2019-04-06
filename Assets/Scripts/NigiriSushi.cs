715e0c88995bb067cdcd8dbf42b3c58bf6c3b327
Grzegorz Kalaska <s15508@pjwstk.edu.pl>
2019-04-06 20:20:58 +0200

    Update NigiriSushi

--- Assets/Scripts/NigiriSushi.cs
+++ Assets/Scripts/NigiriSushi.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class NigiriSushi : Sushi
 {
+    public GameObject slashColliderL;
+    public GameObject slashColliderR;
     // Start is called before the first frame update
     void Start()
     {
         Init();
         this.pointValue = 20;
+        if (isInstance) {
+            if (this.getSelectedTrack().GetComponent<Track>().isLeft)
+            {
+                Destroy(slashColliderL);
+            }
+            else { Destroy(slashColliderR); }
+        }
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/SlashCollider.cs
+++ Assets/Scripts/SlashCollider.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class SlashCollider : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -28,5 +30,6 @@ public class SlashCollider : MonoBehaviour
         }
     }
     public void Stagger() {
+        Debug.Log("Stunned!");
     }
 }
